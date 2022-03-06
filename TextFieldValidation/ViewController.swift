81718f8210260650b29fcf21b4aabbe452798c75
Cong Le <swiftan6@swiftandbeyond.com>
2022-03-05 19:19:59 -0800

    XCode project without storyboard

---
+++ TextFieldValidation/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  TextFieldValidation
+//
+//  Created by Cong Le on 3/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
