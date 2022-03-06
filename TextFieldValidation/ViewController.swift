9ac7f7e47243bf36b11ef20cb7685cdbe16fd07f
Cong Le <longchike@gmail.com>
2022-03-05 23:59:06 -0800

    - Add customized textField

--- TextFieldValidation/ViewController.swift
+++ TextFieldValidation/ViewController.swift
@@ -7,7 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CustomizedTextField: UITextField {
+    public func setLeftTetView(
+        text: String,
+        textColor: UIColor? = nil,
+        textFont: UIFont? = nil,
+        isLeftLabelAccessible: Bool = true,
+        leftLabelAccessibilityText: String? = nil,
+        shouldAddingLeadingSpace: Bool = true
+    ) {
+        let textFieldView = UILabel(frame: CGRect())
+        textFieldView.text = String(format: "%@%@", shouldAddingLeadingSpace ? "   ": "", text)
+        textFieldView.font = textFont ?? UIFont.systemFont(ofSize: 17)
+        textFieldView.textAlignment = .left
+        textFieldView.textColor = .red
+        textFieldView.sizeToFit()
+        textFieldView.accessibilityLabel = leftLabelAccessibilityText ?? "\(text) Text Field".localized
+        textFieldView.isAccessibilityElement = isLeftLabelAccessible
+        self.leftView = textFieldView
+        self.leftViewMode = .always
+    }
+}
+
+extension String {
+    var localized: String {
+        return  NSLocalizedString(self, comment: "")
+    }
+}
+
+class ViewController: UIViewController{
     lazy var textfield: UITextField = {
         let textFieldFrame = CGRect(x: 20, y: 100, width: 300, height: 40)
         let textfield = UITextField(frame: textFieldFrame)
@@ -22,6 +50,21 @@ class ViewController: UIViewController {
         return textfield
     }()
     
+    lazy var customizedtextfield: CustomizedTextField = {
+        let textFieldFrame = CGRect(x: 20, y: 100, width: 300, height: 40)
+        let textfield = CustomizedTextField(frame: textFieldFrame)
+        textfield.placeholder = "Enter text here"
+        textfield.borderStyle = .roundedRect
+        textfield.autocorrectionType = .no
+        textfield.keyboardType = .default
+        textfield.returnKeyType = .done
+        textfield.clearButtonMode = .whileEditing
+        textfield.contentVerticalAlignment = .center
+        textfield.delegate = self
+        textfield.setLeftTetView(text: "Customized text")
+        return textfield
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .blue
@@ -31,19 +74,23 @@ class ViewController: UIViewController {
     
     func style() {
         textfield.translatesAutoresizingMaskIntoConstraints = false
+        customizedtextfield.translatesAutoresizingMaskIntoConstraints = false
     }
     
     func layout() {
         self.view.addSubview(textfield)
+        self.view.addSubview(customizedtextfield)
         NSLayoutConstraint.activate([
             textfield.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             textfield.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             textfield.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            textfield.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+            textfield.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            customizedtextfield.topAnchor.constraint(equalTo: textfield.bottomAnchor, constant: 20),
+            customizedtextfield.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            customizedtextfield.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
         ])
     }
 }
-
 // MARK: - UITextFieldDelegate
 extension ViewController: UITextFieldDelegate {
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
